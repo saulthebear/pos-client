2e5ec2eeb4e3649b0d2ff19d4aeb34e945e4599d
weston-bailey <wdbailey87@gmail.com>
2022-06-23 10:14:44 -0700

    debug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
-export default function Navbar() {
+import { Link } from 'react-router-dom'
+
+export default function Navbar({ currentUser, handleLogout }) {
 	return (
 		<nav>
-			hello from navbar
+			{/* user always sees this section */}
+			<Link to="/">
+				<p>User App</p>
+			</Link>
+
+			{/* if the user is logged in... */}
+			<Link to="/">
+				<span onClick={handleLogout}>logout</span>
+			</Link>
+
+			<Link to="/profile">
+				profile
+			</Link>
+
+			{/* if the user is not logged in... */}
+			<Link to="/register">
+				register
+			</Link>
+
+			<Link to="/login">
+				login
+			</Link>
 		</nav>
 	)
 }
