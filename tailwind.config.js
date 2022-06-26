7a4bf5c3d95bc35823be1f40e09190f14de28d4b
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-25 18:02:53 -0600

    Add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.{html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        plum: {
+          50: "#f8ecf8",
+          100: "#f3dbec",
+          200: "#e5bada",
+          300: "#d9a2d0",
+          400: "#bf7eae",
+          500: "#b373a3",
+          600: "#a46696",
+          700: "#8e5381",
+          800: "#773769",
+          900: "#6c1c5a",
+        },
+        brand: "#78A1BB",
+      },
+    },
   },
   plugins: [],
 }
