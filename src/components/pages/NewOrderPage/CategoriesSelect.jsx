2ab4fbfedd7567b0ba88fdde7f4f278919400364
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-26 11:41:34 -0600

    Style category buttons

--- src/components/pages/NewOrderPage/CategoriesSelect.jsx
+++ src/components/pages/NewOrderPage/CategoriesSelect.jsx
@@ -1,11 +1,33 @@
 import React from "react"
 import CategoryButton from "./CategoryButton"
 
+const categories = [
+  {
+    id: 2,
+    name: "Food",
+    color: "green",
+  },
+  {
+    id: 3,
+    name: "Snacks",
+    color: "blue",
+  },
+  {
+    id: 4,
+    name: "Desserts",
+    color: "purple",
+  },
+  {
+    id: 5,
+    name: "Drinks",
+    color: "yellow",
+  },
+]
+
 export default function CategoriesSelect() {
-  return (
-    <div className="">
-      <CategoryButton />
-      <CategoryButton />
-    </div>
-  )
+  const categoryButtons = categories.map((category) => {
+    return <CategoryButton key={category.id} {...category} />
+  })
+
+  return <div className="flex w-full gap-2 flex-wrap">{categoryButtons}</div>
 }
--- src/components/pages/NewOrderPage/CategoryButton.jsx
+++ src/components/pages/NewOrderPage/CategoryButton.jsx
@@ -1,5 +1,23 @@
 import React from "react"
+import PropTypes from "prop-types"
+import colors from "../../../helpers/userColors"
 
-export default function CategoryButton() {
-  return <div className="bg-plum-600">CategoryButton</div>
+export default function CategoryButton({ id, name, color }) {
+  return (
+    <button
+      type="button"
+      className="bg-plum-600 w-32 rounded-lg text-white font-semibold px-5 py-3 flex items-center"
+    >
+      <div className="flex items-center gap-1 flex-wrap">
+        <div className={`w-4 h-4 rounded-full bg-${colors[color]}`}></div>
+        <p>{name}</p>
+      </div>
+    </button>
+  )
+}
+
+CategoryButton.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
 }
