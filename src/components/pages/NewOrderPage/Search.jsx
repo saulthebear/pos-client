6af5b144e3dde1facd1cc2a552eb54957e4270b9
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-26 09:20:54 -0600

    Style search bar

--- src/components/pages/NewOrderPage/Search.jsx
+++ src/components/pages/NewOrderPage/Search.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 export default function Search() {
   return (
-    <div>
-      <h3>Search Bar</h3>
+    <div className=" relative">
+      <span className="material-symbols-rounded text-plum-600 font-bold absolute right-2 top-1/2 -translate-y-1/2 pointer-events-none">
+        search
+      </span>
       <input
         type="text"
         placeholder="filter items..."
-        className="bg-slate-300 placeholder:text-slate-500"
+        className="bg-slate-300 placeholder:text-slate-500 rounded-md w-full px-4 py-1"
       />
     </div>
   )
