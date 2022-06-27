7fb043def504d40553221a05feea571a70bbb8c3
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-27 10:23:49 -0600

    Add modal component

---
+++ src/components/ui/Modal.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Dialog } from "@headlessui/react"
+
+export function ModalTitle({ children }) {
+  return (
+    <Dialog.Title className="text-3xl text-center font-bold font-red-hat-display">
+      {children}
+    </Dialog.Title>
+  )
+}
+
+ModalTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export function ModalPanel({ children }) {
+  return (
+    <Dialog.Panel className="mx-auto max-w-sm rounded-xl bg-plum-500 text-white min-w-[30rem] min-h-[20rem] p-5">
+      {children}
+    </Dialog.Panel>
+  )
+}
+
+ModalPanel.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default function Modal({ isOpen, setIsOpen, children }) {
+  return (
+    <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+      {/* The backdrop, rendered as a fixed sibling to the panel container */}
+      <div
+        className="fixed inset-0 bg-plum-100/60 backdrop-blur-sm"
+        aria-hidden="true"
+      />
+      {/* Full-screen container to center the panel */}
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        {children}
+      </div>
+    </Dialog>
+  )
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  setIsOpen: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+}
