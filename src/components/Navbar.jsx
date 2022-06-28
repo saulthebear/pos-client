a8f9cb0892160240cccd8f5a3093f6db0aef4fd5
“erhaneth” <gumusucb21@gmail.com>
2022-06-27 17:06:42 -0700

    Update navbar
    Co-authored-by: Stefan Vosloo <saulthebear@users.noreply.github.com>
    Co-authored-by: gracenarez333 <gracenarez333@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Link } from "react-router-dom"
-import { Menu } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react'
 import PropTypes from "prop-types"
 
 export default function Navbar({ currentUser, handleLogout }) {
@@ -24,17 +24,32 @@ export default function Navbar({ currentUser, handleLogout }) {
   )
 
   const admin = (
-    <Menu>
+    <Menu as="span" className="relative">
       <Menu.Button>More</Menu.Button>
-      <Menu.Items>
+      <Transition enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+        className="z-10 absolute"
+        >
+
+      <Menu.Items className="bg-white flex flex-col p-2 rounded">
         <Menu.Item>
           <Link to="/admin/categories">Categories</Link>
-
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/admin/transactions">Transactions</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/admin/employees">Employees</Link>
         </Menu.Item>
         <Menu.Item>
           <Link to="/admin/products">Products</Link>
         </Menu.Item>
       </Menu.Items>
+      </Transition>
     </Menu>
   )
 
@@ -45,8 +60,11 @@ export default function Navbar({ currentUser, handleLogout }) {
       <Link to="/">
         <p>DettiPos</p>
       </Link>
+      {currentUser && currentUser.role === "admin" && admin}
+      {currentUser && loggedIn}
+      {!currentUser && loggedOut}
 
-      {currentUser ? loggedIn : loggedOut}
+      {/* {currentUser ? loggedIn : loggedOut} */}
     </nav>
   )
 }
