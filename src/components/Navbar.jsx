352da9e4ae6d11a8e74be8dc56e2de5e72290509
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-27 21:56:16 -0600

    Style navbar
    Co-authored-by: gracenarez333 <gracenarez333@users.noreply.github.com>
    Co-authored-by: Heg <erhaneth@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Link } from "react-router-dom"
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, Transition } from "@headlessui/react"
 import PropTypes from "prop-types"
 
 export default function Navbar({ currentUser, handleLogout }) {
@@ -26,45 +26,46 @@ export default function Navbar({ currentUser, handleLogout }) {
   const admin = (
     <Menu as="span" className="relative">
       <Menu.Button>More</Menu.Button>
-      <Transition enter="transition duration-100 ease-out"
+      <Transition
+        enter="transition duration-100 ease-out"
         enterFrom="transform scale-95 opacity-0"
         enterTo="transform scale-100 opacity-100"
         leave="transition duration-75 ease-out"
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
         className="z-10 absolute"
-        >
-
-      <Menu.Items className="bg-white flex flex-col p-2 rounded">
-        <Menu.Item>
-          <Link to="/admin/categories">Categories</Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/admin/transactions">Transactions</Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/admin/employees">Employees</Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link to="/admin/products">Products</Link>
-        </Menu.Item>
-      </Menu.Items>
+      >
+        <Menu.Items className="bg-brand shadow-md flex flex-col p-2 rounded">
+          <Menu.Item>
+            <Link to="/admin/categories">Categories</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/admin/transactions">Transactions</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/admin/employees">Employees</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/admin/products">Products</Link>
+          </Menu.Item>
+        </Menu.Items>
       </Transition>
     </Menu>
   )
 
-
   return (
-    <nav>
+    <nav className="p-3 flex justify-between text-white text-lg items-center">
       {/* user always sees this section */}
       <Link to="/">
-        <p>DettiPos</p>
+        <p className=" font-comfortaa font-bold text-2xl text-white">
+          dettiPOS
+        </p>
       </Link>
-      {currentUser && currentUser.role === "admin" && admin}
-      {currentUser && loggedIn}
-      {!currentUser && loggedOut}
-
-      {/* {currentUser ? loggedIn : loggedOut} */}
+      <div className="flex gap-3">
+        {currentUser && currentUser.role === "admin" && admin}
+        {currentUser && loggedIn}
+        {!currentUser && loggedOut}
+      </div>
     </nav>
   )
 }
