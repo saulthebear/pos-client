13a12136521040a0e509c66332c02c7798fff5ad
gracenarez333 <gracemnarez@gmail.com>
2022-06-29 10:27:23 -0700

    adds info to welcome page

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link } from "react-router-dom"
-
 export default function Welcome() {
   return (
     <>
-
+      <h1>DettiPOS is here to help with all our your business needs</h1>
+      <h3>Register as a business and start building your personalized system</h3>
+      <h3>Or register as an employee and start handling transactions</h3>
     </>
   )
 }
