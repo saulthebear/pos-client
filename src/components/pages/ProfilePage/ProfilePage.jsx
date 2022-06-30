33f192ac5a5e6dddb8c8e66bedb693e35086d063
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-30 13:31:12 -0600

    Style Profile Page
    Co-authored-by: Heg <erhaneth@users.noreply.github.com>
    Co-authored-by: gracenarez333 <gracenarez333@users.noreply.github.com>

--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
 import { Navigate } from "react-router-dom"
 import axios from "axios"
-import CashierTransactions from "./CashierTransactions"
 import { getAuthOptions } from "../../../helpers/utils"
-import { useAuth } from "../../../hooks/useAuth"
 import AuthService from "../../../helpers/authServices"
+import UserIcon from "../../ui/UserIcon"
+import Modal, { ModalPanel, ModalTitle } from "../../ui/Modal"
+import { PinkInput } from "../../ui/Input"
+import { ButtonLarge, ModalButton } from "../../ui/Button"
 
 export default function Profile() {
   // const { user: currentUser } = useAuth()
@@ -51,12 +53,71 @@ export default function Profile() {
     }
   }
   return (
-    <div>
+    <div className="p-5">
       <p className="text-red-700">{error}</p>
-      <h1>Profile Page</h1>
-      <p>{currentUser.username}</p>
-      <p>{currentUser.role}</p>
-      {isEditing && (
+      <h1 className="text-3xl font-semibold mb-5">Profile Page</h1>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-xl mb-3">Welcome, {currentUser.username}.</div>
+        <div className="bg-slate-200 rounded-lg w-fit p-5 flex flex-col items-center gap-2">
+          <p className="font-semibold font-xl">Permissions:</p>
+          {currentUser.role === "cashier" && (
+            <>
+              <UserIcon isAdmin={false} />
+              You are a Cashier
+            </>
+          )}
+
+          {currentUser.role === "admin" && (
+            <>
+              <UserIcon isAdmin={true} />
+              You are an Admin
+            </>
+          )}
+
+          {currentUser.role === "unverified" && (
+            <>
+              <p>{"Your account hasn't been activated."}</p>{" "}
+              <p>Contact your manager.</p>
+            </>
+          )}
+          <ButtonLarge
+            onClick={() => setIsEditing(!isEditing)}
+            className="bg-plum-400 text-white"
+          >
+            {isEditing ? "Cancel" : "Edit Login Info"}
+          </ButtonLarge>
+        </div>
+      </div>
+
+      <Modal isOpen={isEditing} setIsOpen={setIsEditing}>
+        <ModalPanel>
+          <ModalTitle>Edit Profile</ModalTitle>
+          <form
+            onSubmit={(e) => handleSubmit(e, form, setForm)}
+            className="flex flex-col justify-between"
+          >
+            <PinkInput
+              name="username"
+              label="Username"
+              value={form.username}
+              type="text"
+              onChange={(e) => setForm({ ...form, username: e.target.value })}
+            />
+            <PinkInput
+              name="password"
+              label="Password"
+              value={form.password}
+              type="password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+            />
+            <div className="flex justify-end">
+              <ModalButton type="submit">Update</ModalButton>
+            </div>
+          </form>
+        </ModalPanel>
+      </Modal>
+
+      {/* {isEditing && (
         <form onSubmit={(e) => handleSubmit(e, form, setForm)}>
           <label htmlFor="username">Username:</label>
           <input
@@ -74,12 +135,7 @@ export default function Profile() {
           />
           <button type="submit">Update</button>
         </form>
-      )}
-      <button onClick={() => setIsEditing(!isEditing)}>
-        {isEditing ? "Cancel" : "Edit"}
-      </button>
-
-      <CashierTransactions />
+      )} */}
     </div>
   )
 }
