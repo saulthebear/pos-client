f6fc3638496cd951ab80585bda082f76629497fb
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-30 22:49:48 -0600

    Add form validation and tweak styling

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -6,6 +6,7 @@ import { ModalButton } from "../ui/Button"
 import PropTypes from "prop-types"
 import AuthService from "../../helpers/authServices"
 import { userShape } from "../../helpers/propTypes"
+import { ModalTitle } from "../ui/Modal"
 
 export default function Register({ currentUser, setCurrentUser }) {
   // const { register, user, error } = useAuth()
@@ -13,6 +14,7 @@ export default function Register({ currentUser, setCurrentUser }) {
   const [error, setError] = useState("")
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [formError, setFormError] = useState("")
 
   const register = AuthService.register
   let navigate = useNavigate()
@@ -20,6 +22,9 @@ export default function Register({ currentUser, setCurrentUser }) {
   // submit event handler
   const handleSubmit = async (e) => {
     e.preventDefault()
+    if (!validateForm()) {
+      return
+    }
     const response = await register(username, password)
     if (response.user) {
       setCurrentUser(response.user)
@@ -33,17 +38,38 @@ export default function Register({ currentUser, setCurrentUser }) {
     console.log("currentUser", currentUser)
     return <Navigate to="/profile" />
   }
+  const validateForm = () => {
+    if (username == "") {
+      setFormError("Name is required!")
+      return false
+    }
+    if (password == "") {
+      setFormError("Password is required!")
+      return false
+    }
+    return true
+  }
 
   return (
     <div className="bg-plum-50 flex justify-between items-center px-12 py-12 h-full">
       <div className="bg-plum-500 mx-auto px-14 py-6 rounded-3xl shadow-md pt-12">
-        <h1 className="font-xl font-bold text-white px-16">
-          Register for an account:
+        {/* <ModalTitle>Register for an Account</ModalTitle> */}
+        <h1 className="text-3xl text-center font-bold font-red-hat-display mb-5 text-white">
+          Register for an Account
         </h1>
 
-        <p className="text-red-700">{error}</p>
+        <p className="flex items-center justify-center text-plum-900 bg-plum-200 py-2 px-5 rounded-lg">
+          <span className="material-symbols-rounded mr-3">info</span>
+          <span>
+            Your account will need to be approved by a manager, before you can
+            handle transactions.
+          </span>
+        </p>
 
-        <form onSubmit={handleSubmit}>
+        <p className="text-red-700">{error}</p>
+        <p className="text-red-700 pb-4">{formError}</p>
+
+        <form onSubmit={handleSubmit} className="">
           <PinkInput
             label="Username:"
             type="text"
